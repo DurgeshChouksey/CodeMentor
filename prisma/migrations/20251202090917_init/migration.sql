46a13119abd920619285ee9004155b7fd66456a7
GP <gouravpatil492@gmail.com>
2025-12-02 14:49:24 +0530

    feat: update Prisma configuration and migrations; add User, RepoSync, and RepoJob tables

--- prisma/migrations/20251202090917_init/migration.sql
+++ prisma/migrations/20251202090917_init/migration.sql
@@ -23,8 +23,38 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "RepoSync" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "owner" TEXT NOT NULL,
+    "repo" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "lastCommitSha" TEXT NOT NULL,
+    "s3Key" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "RepoSync_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RepoJob" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "repoId" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'processing',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "RepoJob_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_githubId_key" ON "User"("githubId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RepoSync_fullName_key" ON "RepoSync"("fullName");
